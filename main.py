ca8ce2528247e72acc50ba1a2636bf37b8206941
AntoniovanDijck <antonio.vdijck@gmail.com>
2024-03-21 19:07:24 +0100

    updated readme

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def main():
     # FOR TRAINING UNCOMMENT THE FOLLOWING LINES:
 
     # print(f"\nTraining the agent for {training_rounds} rounds:")
-    # training_rounds = 1000
+    # training_rounds = 10000
     # train_agent(training_rounds)
 
     # TESTING THE LATEST WEIGHTS
     print("\nTesting the trained agent with detailed metrics and plotting performance:")
-    testing_rounds = 100
+    testing_rounds = 1000
     test_agent(rounds=testing_rounds, report_interval=1)
 
 if __name__ == "__main__":
